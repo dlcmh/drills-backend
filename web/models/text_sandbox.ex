a7b1fa04630726a1168e1fa9e8965bf82f9def1b
David Chin <dlcmhd@me.com>
2017-05-02 14:12:06 +0800

    String processing

--- web/models/text_sandbox.ex
+++ web/models/text_sandbox.ex
@@ -12,10 +12,28 @@ defmodule Drills.TextSandbox do
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
-    IO.inspect params
     struct
     |> cast(params, [:raw])
     |> validate_required([:raw])
-    |> put_change(:parsed, String.trim(params["raw"] || ""))
+    |> put_change(:parsed, process_text(params["raw"]))
+  end
+
+  defp process_text(text) do
+    if text == nil do
+      ""
+    else
+      text
+      |> String.split("\n")
+      |> Enum.map(fn line -> String.strip(line) end)
+      |> Enum.map(fn line -> clean_each_cell_in_line(line) end)
+      |> Enum.join("\n")
+    end
+  end
+
+  defp clean_each_cell_in_line(line) do
+    line
+    |> String.split("\t")
+    |> Enum.map(fn string -> String.strip(string) end)
+    |> Enum.join("\t")
   end
 end
